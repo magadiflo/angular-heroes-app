0ec7ee39df79e01190ed51e11633e0503f536c02
Martín <magadiflo@gmail.com>
2022-08-13 00:10:04 -0500

    Mostrar Rutas Hijas - Segundo RouterOutlet

--- src/app/heroes/heroes-routing.module.ts
+++ src/app/heroes/heroes-routing.module.ts
@@ -5,11 +5,13 @@ import { ListComponent } from './pages/list/list.component';
 import { AddComponent } from './pages/add/add.component';
 import { SearchComponent } from './pages/search/search.component';
 import { HeroComponent } from './pages/hero/hero.component';
+import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
   {
     path: '',
-    children: [
+    component: HomeComponent, //* En este momento, este componente se desplegará en la ruta padre (/heroes) para estas rutas hijas
+    children: [ //* Para mostrar los componentes asociados a estas rutas hijas, en el HomeComponent.html debemos usar la directiva <router-outlet></router-outlet>
       { path: 'list', component: ListComponent, },
       { path: 'add', component: AddComponent, },
       { path: 'edit/:id', component: AddComponent, },
