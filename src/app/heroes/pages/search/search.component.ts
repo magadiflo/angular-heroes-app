a12c426b22f925e676e2ae034e7f7b46474ea523
Martín <magadiflo@gmail.com>
2022-08-18 22:54:37 -0500

    Material Autocomplete

--- src/app/heroes/pages/search/search.component.ts
+++ src/app/heroes/pages/search/search.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Hero } from '../../interfaces/heroes.interface';
+import { HeroesService } from '../../services/heroes.service';
+
+
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
@@ -8,9 +12,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  term: string = '';
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroesService) { }
 
   ngOnInit(): void {
+  }
+
+  searching() {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
   }
 
 }
