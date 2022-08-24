6325f10e5e047ae4c6ea57b67c44657e5ed89d6f
Martín <magadiflo@gmail.com>
2022-08-23 22:30:52 -0500

    Pantalla de Login Básico

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import { MaterialModule } from '../material/material.module';
 
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
@@ -15,6 +16,7 @@ import { RegisterComponent } from './pages/register/register.component';
   imports: [
     CommonModule,
     AuthRoutingModule,
+    MaterialModule,
   ]
 })
 export class AuthModule { }
